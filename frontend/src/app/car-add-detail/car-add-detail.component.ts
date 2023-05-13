d6f2925f7d829bdc72089fca88656324e5a4fed9
tukitoki <tokichiihere@gmail.com>
2023-05-13 09:42:08 +0300

    add files

---
+++ frontend/src/app/car-add-detail/car-add-detail.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { DetailService } from '../service/detail.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DetailDto } from '../entity/DetailDto';
+import { CarAddDetailsRequest } from '../entity/CarAddDetailsRequest';
+import { CarService } from '../service/car.service';
+
+@Component({
+  selector: 'app-car-add-detail',
+  templateUrl: './car-add-detail.component.html',
+  styleUrls: ['./car-add-detail.component.css']
+})
+export class CarAddDetailComponent {
+
+  details!: DetailDto[];
+  detailToSend!: CarAddDetailsRequest;
+  currIndex: number = 0;
+  selected!: boolean;
+  carId!: number;
+
+  constructor(private detailService: DetailService,
+    private carService: CarService,
+    private router: Router,
+    private activatedRt: ActivatedRoute) {
+}
+
+  ngOnInit(): void {
+    this.detailService.getAllDetails().subscribe({
+      next: value => this.details = value
+    })
+    this.activatedRt.queryParams.subscribe(params => {
+      this.carId = params['id']
+    })
+    this.carService.getCarDetails(this.carId).subscribe({
+      next: value => {
+        for (let i = 0; i < value.length; i++) {
+          let ind = this.details.map(e => e.id).indexOf(value[i].id);
+          if (ind != -1) {
+            this.details.splice(ind, 1);
+          }
+        }
+      }
+    })
+    this.detailToSend = {
+      detailIds: []
+    }
+  }
+
+  toDetail(id: number | null, event: MouseEvent) {
+    event.preventDefault();
+    window.open("http://localhost:4200/api/detail/" + id);
+  }
+
+  checkSelected(event: any, id: number | null) {
+    this.selected = (event.target as HTMLInputElement).checked;
+    if (this.selected) {
+      this.detailToSend.detailIds.push(id);
+    } else {
+      this.detailToSend.detailIds.splice(this.detailToSend.detailIds.indexOf(id), 1);
+    }
+  }
+
+  ifPresent(id: number | null) {
+    return this.detailToSend.detailIds.indexOf(id) != -1
+  }
+
+  sendDetails() {
+    this.carService.addDetailsToCar(this.carId, this.detailToSend).subscribe();
+    this.router.navigate([`api/cars`])
+  }
+}
