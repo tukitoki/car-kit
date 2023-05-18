242f4655813fe8de57261f2e42298bec35926d96
tukitoki <tokichiihere@gmail.com>
2023-05-18 22:17:45 +0300

    demo ci

--- backend/src/main/java/cs/vsu/raspopov/carkit/config/SecurityConfig.java
+++ backend/src/main/java/cs/vsu/raspopov/carkit/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
 
